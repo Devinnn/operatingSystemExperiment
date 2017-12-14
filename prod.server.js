20004bdd6190084ccc9053286e5e6c897aaa8636
Devin <devinnan@163.com>
2017-12-14 16:45:37 +0800

    修复history模式下在地址栏输入url请求不到的问题。

--- prod.server.js
+++ prod.server.js
@@ -1,9 +1,11 @@
 // 内建服务器
 var express = require('express');
 var config = require('./config/index');
+var history = require('connect-history-api-fallback');
 
 var app = express();
 
+app.use(history());
 app.use(express.static('./dist'));
 
 var port = process.env.PORT || config.build.port;
